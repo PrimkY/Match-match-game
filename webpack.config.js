8af05bd6930c0973c4bdb0da59ab9a04c33cbd2d
PrimkY <fakeprimky@gmail.com>
2022-06-19 16:10:32 +0300

    init: created webpack.config and working area

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+const esLintPlugin = (isDev) => isDev ? [] : [ new ESLintPlugin({ extensions: ['ts', 'js'] }) ];
+
+const devServer = (isDev) => !isDev ? {} : {
+    devServer: {
+        open: true,
+        port: 8080,
+    },
+};
+
+module.exports = ({ development }) => ({
+    mode: development ? 'development' : 'production',
+    devtool: development ? 'inline-source-map' : false,
+    entry: './src/index.ts',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(csv|tsv)$/i,
+                use: ['csv-loader'],
+            },
+            {
+                test: /\.xml$/i,
+                use: ['xml-loader'],
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: [
+        ...esLintPlugin(development),
+        new HtmlWebpackPlugin({template: './src/index.html'}),
+        new MiniCssExtractPlugin({ filename: '[name].css' }),
+        new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    ],
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    ...devServer(development)
+});
